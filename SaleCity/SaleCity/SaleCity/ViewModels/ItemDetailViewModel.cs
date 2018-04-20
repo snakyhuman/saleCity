3906d005aeba08abfbddc2d45643ae346a56eb40
snakyhuman <snakyhuman@gmail.com>
2018-04-20 12:41:57 +0700

    first

---
+++ SaleCity/SaleCity/SaleCity/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using SaleCity.Models;
+
+namespace SaleCity.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
---
+++ SaleCity/SaleCity/SaleCity/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using SaleCity.Models;
+using SaleCity.ViewModels;
+
+namespace SaleCity.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ItemDetailPage : ContentPage
+	{
+        ItemDetailViewModel viewModel;
+
+        public ItemDetailPage(ItemDetailViewModel viewModel)
+        {
+            InitializeComponent();
+
+            BindingContext = this.viewModel = viewModel;
+        }
+
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+
+            var item = new Item
+            {
+                Text = "Item 1",
+                Description = "This is an item description."
+            };
+
+            viewModel = new ItemDetailViewModel(item);
+            BindingContext = viewModel;
+        }
+    }
+}
